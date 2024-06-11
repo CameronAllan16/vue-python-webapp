b020bb50bf5d63f3a5f0bfc7fac70de50ee45361
Cameron Allan <camallan16@gmail.com>
2024-06-11 14:00:29 -0400

    Add a cute little alert for confirmation of book being added

---
+++ client/src/components/AlertComponent.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+    props: ['message'],
+};
+</script>
+<template>
+    <div>
+        <div class="alert alert-success" role="alert"> {{ message }}</div>
+        <br />
+    </div>
+
+</template>
